445342e2daadc0b654c8b16762f8500b727cee4d
RezaIbnuH <rezaibnu752@gmail.com>
2024-04-02 23:06:51 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+
+    Saat ini saya kuliah di salah satu Kampus swasta di Riau yaitu <a href="https://uir.ac.id/" target="_blank">Universitas Islam Riau</a>. Sebelumnya saya bersekolah di <a href="https://smanegeri1kubu.sch.id/#" target="_blank">SMA NEGERI 1 KUBU</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Tempat, Tanggal Lahir</template>
+
+    Saya lahir di sebuah kecamatan kubu jln. datuk kancil Kabupaten <a href="https://id.wikipedia.org/wiki/Kabupaten_Rokan_Hilir" target="_blank">Rokan Hilir</a> Provinsi<a href="https://id.wikipedia.org/wiki/Riau" target="_blank">Riau</a> pada tanggal 7 Juni 2004.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Jurusan</template>
+
+    Prodi atau Jurusan Kuliah Saya mengambil Program didik Teknik Informatika atau biasa disebut IT , dan saya sekarang berada di semester 4
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+    <ul>
+      <li>Futsal</li>
+      <li>Sepak bola</li>
+      <li>Badminton</li>
+      <li>Voly</li>
+      <li>Renang</li>
+      <li>Lari</li>
+      <li>Gitar</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Social Media</template>
+    
+    <ul>
+      <li>Instagram   : <a href="https://www.instagram.com/nanda_alfiranda/#" target="_blank">Nanda Alfiranda</a></li>
+      <li>Facebook    : <a href="https://www.facebook.com/Nanda.alfiranda.984" target="_blank">Nanda Alfiranda</a></li>
+      <li>WhatsApp    : 0897-7476-390</li>
+    </ul>
+  </WelcomeItem>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>Ini adalah website Profil saya, dan website ini adalah tugas pertama dari mata kuliah praktikum Pemrograman berbasis Komponen (PBK) yang di ajarakan langsung oleh 2 Asdos yang baik hati, tampan dan pemberani yaitu bang Hanif dan bang Tegar.</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
